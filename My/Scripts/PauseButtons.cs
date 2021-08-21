e8cf3a016bc263b2f5ac95958786f4bfc68a981a
ArtikMix <a.katkovski5@gmail.com>
2021-08-21 23:40:16 +0300

    ui+fiches

--- My/Scripts/PauseButtons.cs
+++ My/Scripts/PauseButtons.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject panel;
+    public void Pause()
     {
-        
+        Time.timeScale = 0f;
+        panel.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Resume()
     {
-        
+        panel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
