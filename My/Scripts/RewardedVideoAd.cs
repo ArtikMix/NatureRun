fc7bb07806eb2daadadc3ff3e94eb70c7248ac61
ArtikMix <a.katkovski5@gmail.com>
2021-08-28 19:44:39 +0300

    rearded video

---
+++ My/Scripts/RewardedVideoAd.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+public class RewardedVideoAd : MonoBehaviour, IUnityAdsListener
+{
+    [SerializeField] private bool _testMode = true;
+    [SerializeField] private Button _adsButton;
+
+    private string _gameId = "4276191";
+    private string _rewardedVideo = "Rewarded_Android";
+
+    void Start()
+    {
+        _adsButton = GetComponent<Button>();
+        _adsButton.interactable = Advertisement.IsReady(_rewardedVideo);
+        if (_adsButton)
+            _adsButton.onClick.AddListener(ShowRewardedVideo);
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(_gameId, true);
+    }
+
+    public void ShowRewardedVideo()
+    {
+        Advertisement.Show(_rewardedVideo);
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        if (placementId == _rewardedVideo)
+        {
+            _adsButton.interactable = true;
+        }
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        //
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        //
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if (showResult == ShowResult.Finished)
+        {
+            FindObjectOfType<CoinLogic>().coins += 20;
+            PlayerPrefs.SetInt("coins", FindObjectOfType<CoinLogic>().coins);
+            PlayerPrefs.Save();
+        }
+        else if (showResult == ShowResult.Skipped)
+        {
+            //
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+            //
+        }
+    }
+}
